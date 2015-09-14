5dd8464b4e6da2850932092a487d9d39eba9bf69
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-14 20:51:11 +0200

    Some basic styling.

--- schwifty/endpoints.py
+++ schwifty/endpoints.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, jsonify
 
 from schwifty.core import app
+
+# TODO: support OAuth against ID
+# TODO: make notes, bookmarks, links
+
+
+@app.route('/api/entity/<doc_type>/<id>')
+def entity(doc_type, id):
+    return jsonify({'status': 'ok'})
 
 
 @app.route('/')
 def index():
-    return "hello, world!"
+    return render_template('index.html')
