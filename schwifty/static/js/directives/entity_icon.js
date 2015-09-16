df8f6f800cea95835d9ecefebe9cff6bf41883b5
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-16 10:31:41 +0200

    Show icons for different entity types.

---
+++ schwifty/static/js/directives/entity_icon.js
@@ -0,0 +1,29 @@
+
+schwifty.directive('entityIcon', ['$http', function($http) {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      'schema': '='
+    },
+    templateUrl: 'entity_icon.html',
+    link: function (scope, element, attrs, model) {
+
+      scope.isPerson = function() {
+        return scope.schema == 'http://schema.occrp.org/generic/person.json#';
+      }
+
+      scope.isOrganization = function() {
+        return scope.schema == 'http://schema.occrp.org/generic/organization.json#';
+      }
+
+      scope.isCompany = function() {
+        return scope.schema == 'http://schema.occrp.org/generic/company.json#';
+      }
+
+      scope.isOther = function() {
+        return !scope.isPerson() && !scope.isOrganization() && !isCompany();
+      }
+    }
+  };
+}]);
