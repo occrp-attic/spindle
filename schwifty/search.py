9e80f90add88cecaefdae3ae6a687cdc11a7f2ab
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-16 14:57:20 +0200

    Very basic support for facets.

--- schwifty/search.py
+++ schwifty/search.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from pprint import pprint  # noqa
+
 from flask import url_for
 from werkzeug.datastructures import MultiDict
 
@@ -6,15 +9,44 @@ from schwifty.core import es, es_index
 QUERY_FIELDS = ['raw.*']
 DEFAULT_FIELDS = ['source.*', 'id', 'schema', 'entity.*']
 
+# Scoped facets are facets where the returned facet values are returned such
+# that any filter against the same field will not be applied in the sub-query
+# used to generate the facet values.
+SCOPED_FACETS = ['schema', 'source.slug']
+
 
 def query(args):
     """ Parse a user query string, compose and execute a query. """
     if not isinstance(args, MultiDict):
         args = MultiDict(args)
     q = text_query(args.get('q'))
+
+    facets = args.getlist('facet')
+    print 'FACETS', facets
+    # TODO: filters on jurisdiction, source, schema
+    # semantics: source, schema facets are unaffected by source & schema
+    #            filters
+
+    aggs = {
+        'scoped': {
+            'global': {},
+            'aggs': {}
+        }
+    }
+    for facet in facets:
+        agg = {facet: {'terms': {'field': facet}}}
+        if facet in SCOPED_FACETS:
+            aggs['scoped']['aggs'][facet] = {
+                'filter': {'query': q},
+                'aggs': agg
+            }
+        else:
+            aggs.update(agg)
+
+    # TODO: return facets for the above.
     q = {
         'query': q,
-        'aggregations': {},
+        'aggregations': aggs,
         '_source': DEFAULT_FIELDS
     }
 
@@ -31,7 +63,27 @@ def query(args):
         # When requesting a second page of the results, the client will not
         # need to be returned facet results a second time.
         del q['aggregations']
-    return execute_query(q)
+
+    return execute_query(q, facets)
+
+
+def add_filter(q, filter_):
+    """ Add the filter ``filter_`` to the given query. """
+    q = deepcopy(q)
+    if 'filtered' not in q:
+        return {
+            'filtered': {
+                'query': q,
+                'filter': filter_
+            }
+        }
+
+    if 'and' in q['filtered']['filter']:
+        q['filtered']['filter']['and'].append(filter_)
+    else:
+        q['filtered']['filter'] = \
+            {'and': [filter_, q['filtered']['filter']]}
+    return q
 
 
 def text_query(text):
@@ -64,7 +116,7 @@ def text_query(text):
     return q
 
 
-def execute_query(q):
+def execute_query(q, facets):
     """ Execute the query and return a set of results. """
     result = es.search(index=es_index, body=q)
     hits = result.get('hits', {})
@@ -73,11 +125,25 @@ def execute_query(q):
         'results': [],
         'offset': q['from'],
         'limit': q['size'],
-        'total':  hits.get('total')
+        'took': result.get('took'),
+        'total': hits.get('total'),
+        'facets': {}
     }
     for doc in hits.get('hits', []):
         data = doc.get('_source')
         data['uri'] = url_for('entity', doc_type=doc.get('_type'),
                               id=doc.get('_id'), _external=True)
         output['results'].append(data)
+
+    # traverse and get all facets.
+    aggs = result.get('aggregations')
+    for facet in facets:
+        scoped = aggs.get('scoped', {}).get(facet, {})
+        value = aggs.get(facet, scoped.get(facet, {}))
+        data = {
+            'total': scoped.get('doc_count', hits.get('total')),
+            'values': value.get('buckets', [])
+        }
+        output['facets'][facet] = data
+
     return output
