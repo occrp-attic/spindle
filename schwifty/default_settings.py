fa988a7de9bcbeab43c1499bdb11c58ade8d6503
Smári McCarthy <smari@mailpile.is>
2015-09-26 16:33:08 +0200

    Split for Schwifty default theme and OCCRP theme.

--- schwifty/default_settings.py
+++ schwifty/default_settings.py
@@ -1,7 +1,9 @@
 DEBUG = True
 ASSETS_DEBUG = False
-APP_NAME = 'search.occrp.org'
+APP_NAME = 'OCCRP Search'
 
-ELASTICSEARCH_HOST = 'localhost:9200'
+ELASTICSEARCH_HOST = '127.0.0.1:9200'
 ELASTICSEARCH_INDEX = 'datavault'
 SOURCE_DOC_TYPE = 'source'
+
+THEME = 'id'
