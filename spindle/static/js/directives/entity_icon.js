eeb0b3c5a3d400a5e0e5b5dccd46ca4e15e49614
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-19 12:08:52 +0200

    Change schema paths.

--- spindle/static/js/directives/entity_icon.js
+++ spindle/static/js/directives/entity_icon.js
@@ -9,19 +9,19 @@ spindle.directive('entityIcon', ['$http', function($http) {
     link: function (scope, element, attrs, model) {
 
       scope.getIcon = function() {
-        if (scope.schema == 'http://schema.occrp.org/generic/person.json#') {
+        if (scope.schema == 'https://schema.occrp.org/generic/person.json#') {
           return 'fa-user';
         }
-        if (scope.schema == 'http://schema.occrp.org/generic/organization.json#') {
+        if (scope.schema == 'https://schema.occrp.org/generic/organization.json#') {
           return 'fa-university';
         }
-        if (scope.schema == 'http://schema.occrp.org/generic/company.json#') {
+        if (scope.schema == 'https://schema.occrp.org/generic/company.json#') {
           return 'fa-suitcase';
         }
-        if (scope.schema == 'http://schema.occrp.org/generic/legal_person.json#') {
+        if (scope.schema == 'https://schema.occrp.org/generic/legal_person.json#') {
           return 'fa-user-secret';
         }
-        if (scope.schema == 'http://schema.occrp.org/generic/land.json#') {
+        if (scope.schema == 'https://schema.occrp.org/generic/land.json#') {
           return 'fa-map';
         }
         return 'fa-question-circle';
