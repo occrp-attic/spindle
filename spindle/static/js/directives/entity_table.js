1de5f998ae50af8766a4800ae6f8fe3a197d1635
Friedrich Lindenberg <friedrich@pudo.org>
2015-10-21 12:25:18 +0200

    Implement pagination on entity tables.

--- spindle/static/js/directives/entity_table.js
+++ spindle/static/js/directives/entity_table.js
@@ -14,6 +14,50 @@ spindle.directive('entityTable', [function() {
       var self = this,
           columns = [];
       $scope.rows = [];
+      $scope.limit = 15;
+      $scope.page = 1;
+      $scope.total = 0;
+
+      $scope.nextPage = function() {
+        if ($scope.pageCount() <= $scope.page) {
+          return null;
+        }
+        return $scope.page + 1;
+      };
+
+      $scope.prevPage = function() {
+        if ($scope.page <= 1) {
+          return null;
+        }
+        return $scope.page - 1;
+      };
+
+      $scope.setPage = function(page) {
+        $scope.page = page;
+      };
+
+      $scope.pages = function() {
+        var range = 3,
+            count = $scope.pageCount(),
+            pages = [],
+            cand = Math.max(1, $scope.page - range),
+            limit = Math.min(count, cand + (range * 2));
+        while (true) {
+          if (cand > 0) {
+            if (cand <= limit) {
+              pages.push(cand);
+            } else {
+              break;
+            }
+          }
+          cand++;
+        }
+        return pages;
+      };
+
+      $scope.pageCount = function() {
+        return Math.ceil($scope.rows.length / $scope.limit);
+      }
 
       self.getColumnBinds = function(row) {
         var binds = [];
@@ -28,18 +72,22 @@ spindle.directive('entityTable', [function() {
         return binds;
       };
 
+      self.updateRows = function() {
+        if ($scope.bind && $scope.bind.has($scope.collection)) {
+          var rows = $scope.bind.p[$scope.collection].binds,
+              boundRows = [];
+          for (var i in rows) {
+            boundRows.push(self.getColumnBinds(rows[i]));
+          }
+          $scope.rows = boundRows;
+        }
+      };
+
       self.addColumn = function(column) {
         columns.push(column);
 
         if (columns.length == parseInt($scope.columns, 10)) {
-          if ($scope.bind && $scope.bind.has($scope.collection)) {
-            var rows = $scope.bind.p[$scope.collection].binds,
-                boundRows = [];
-            for (var i in rows) {
-              boundRows.push(self.getColumnBinds(rows[i]));
-            }
-            $scope.rows = boundRows;
-          }
+          self.updateRows();
         }
       };
     }]
