4b361d42d8617de13c4f07ab3e69add5473399be
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-05 10:16:55 +0100

    Collections settings screen.

--- spindle/static/js/controllers/collections.js
+++ spindle/static/js/controllers/collections.js
@@ -45,8 +45,49 @@ var loadCollection = ['$q', '$http', '$route', function($q, $http, $route) {
 }];
 
 
-spindle.controller('CollectionController', ['$scope', '$http', 'collection',
-    function($scope, $http, collection) {
+spindle.controller('CollectionController', ['$scope', '$http', '$uibModal', 'collection',
+    function($scope, $http, $uibModal, collection) {
   $scope.collection = collection;
   console.log("Collection", collection);
+
+  $scope.editSettings = function() {
+    var d = $uibModal.open({
+      templateUrl: 'collections/settings.html',
+      controller: 'CollectionSettingsDialog',
+      backdrop: true,
+      resolve: {
+        collection: function() {
+          return angular.copy($scope.collection);
+        }
+      }
+    });
+
+    d.result.then(function(collection) {
+      $scope.collection = collection;
+    });
+  };
+}]);
+
+
+spindle.controller('CollectionSettingsDialog', ['$scope', '$http', '$uibModalInstance', 'collection',
+    function($scope, $http, $uibModalInstance, collection) {
+  $scope.collection = collection;
+
+  $scope.validTitle = function() {
+    return $scope.collection.title.length > 2;
+  }
+
+  $scope.save = function() {
+    if ($scope.validTitle()) {
+      var apiUrl = '/api/collections/' + $scope.collection.id;
+      $http.post(apiUrl, $scope.collection).then(function(res) {
+        $uibModalInstance.close(res.data.data);
+      });
+    }
+  };
+
+  $scope.close = function() {
+    $uibModalInstance.dismiss('ok');
+  };
+
 }]);
