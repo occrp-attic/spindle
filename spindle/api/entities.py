2b0ef26ace01cce1374ae78bf81166a49d16a65a
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-06 12:57:17 +0100

    Ultra-simple entity API

--- spindle/api/entities.py
+++ spindle/api/entities.py
@@ -4,6 +4,7 @@ from apikit import jsonify, obj_or_404
 from spindle import authz
 from spindle.core import get_loom_config
 from spindle.util import result_entity
+from spindle.api.collections import get_collection
 
 entities_api = Blueprint('entities', __name__)
 
@@ -13,3 +14,19 @@ def view(id):
     entities = get_loom_config().entities
     data = obj_or_404(entities.get(id, depth=3, right=authz.entity_right()))
     return jsonify({'status': 'ok', 'data': result_entity(data)})
+
+
+@entities_api.route('/api/collections/<int:collection>/entities')
+def collection_index(collection):
+    collection = get_collection(collection, authz.READ)
+    entities = get_loom_config().entities
+    # FIXME: this is a performance nightmare. Think about how to fix it.
+    results = []
+    for cs in collection.subjects:
+        schema = entities.get_schema(cs.subject, right=authz.entity_right())
+        data = entities.get(cs.subject, schema=schema, depth=2,
+                            right=authz.entity_right())
+        results.append(data)
+    return jsonify({
+        'results': results
+    })
--- spindle/tests/test_entities_api.py
+++ spindle/tests/test_entities_api.py
@@ -1,4 +1,4 @@
-from loom.db import Collection, session
+from loom.db import Collection, CollectionSubject, session
 from spindle.model import Permission, Role
 from spindle.tests.util import TestCase
 
@@ -18,11 +18,18 @@ class EntitiesApiTestCase(TestCase):
         permission.resource_id = self.coll.id
         permission.resource_type = Permission.COLLECTION
         session.add(permission)
-        session.commit()
         self.schema_uri = 'https://schema.occrp.org/generic/organization.json#'
         self.entity = {'id': 'foo', 'name': 'Foobar'}
         self.config.entities.save(self.schema_uri, self.entity,
                                   collection_id=self.coll.id)
+        cs = CollectionSubject(self.coll, self.entity['id'])
+        session.add(cs)
+        entity = {'id': 'test', 'name': 'Testing'}
+        self.config.entities.save(self.schema_uri, entity,
+                                  collection_id=self.coll.id)
+        cs = CollectionSubject(self.coll, entity['id'])
+        session.add(cs)
+        session.commit()
 
     def test_view(self):
         res = self.client.get('/api/entities/foo')
@@ -38,4 +45,18 @@ class EntitiesApiTestCase(TestCase):
     def test_view_not_found(self):
         self.login()
         res = self.client.get('/api/entities/foobar')
-        assert res.status_code == 404
+        assert res.status_code == 404, res
+
+    def test_collection_index(self):
+        url = '/api/collections/%s/entities' % self.coll.id
+        res = self.client.get(url)
+        assert res.status_code == 403, res
+
+        self.login()
+        url = '/api/collections/%s/entities' % self.coll.id
+        res = self.client.get(url)
+        assert res.status_code == 200, res
+        results = res.json['results']
+        assert len(results) == 2, res.json
+        assert 'name' in results[0], res.json
+        assert '$schema' in results[0], res.json
