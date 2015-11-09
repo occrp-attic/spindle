2cae9250a4215d8dd7c3ba80ab225a028294b0ec
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-09 14:12:38 +0100

    Fix up WSGI usage.

--- spindle/cli.py
+++ spindle/cli.py
@@ -23,19 +23,21 @@ def configure_app(config={}):
     return app
 
 
+app = configure_app()
+manager = Manager(app)
+manager.add_command('assets', ManageAssets(assets))
+
+
+@manager.command
+def init():
+    "Initialize all parts of the system"
+    indexer = get_loom_indexer()
+    indexer.configure()
+    Role.create_defaults()
+    session.commit()
+
+
 def main():
-    app = configure_app()
-    manager = Manager(app)
-    manager.add_command('assets', ManageAssets(assets))
-
-    @manager.command
-    def init():
-        "Initialize all parts of the system"
-        indexer = get_loom_indexer()
-        indexer.configure()
-        Role.create_defaults()
-        session.commit()
-
     manager.run()
 
 
