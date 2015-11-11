888a62a3882c735a5a0d570f092a782a25bb1b0e
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-11 09:21:13 +0100

    Switch to building a new table grid.

--- spindle/static/js/directives/entity_editor.js
+++ spindle/static/js/directives/entity_editor.js
@@ -11,17 +11,7 @@ spindle.directive('entityEditor', ['$http', 'authz', 'hotRegisterer', 'metadataS
     templateUrl: 'entities/editor.html',
     controller: ['$scope', function($scope) {
       var apiUrl = '/api/collections/' + $scope.collection.id + '/entities';
-
       $scope.editable = authz.collection(authz.WRITE, $scope.collection.id);
-      $scope.editorId = 'table' + $scope.model.schema.id;
-      $scope.tableSettings = {
-        stretchH: 'all',
-        fixedColumnsLeft: 0,
-        minSpareRows: 20,
-        // manualColumnMove: true,
-        colWidths: 170,
-        startRows: 20,
-      };
 
       var loadData = function() {
         var params = {$schema: $scope.model.schema.id};
@@ -30,53 +20,65 @@ spindle.directive('entityEditor', ['$http', 'authz', 'hotRegisterer', 'metadataS
         });
       };
 
+      // var getModelColumns = function(model, metadata) {
+      //   var columns = [],
+      //       properties = model.getPropertyModels().sort(spindleModelSort);
+      //   for (var i in properties) {
+      //     var prop = properties[i];
+      //     if (prop.schema.hidden || prop.isArray) {
+      //       continue;
+      //     }
+      //     var column = {
+      //       title: prop.getTitle(),
+      //       data: prop.name,
+      //       readOnly: !$scope.editable
+      //     };
+      //     if (prop.isValue) {
+      //       if (prop.types.indexOf('integer') != -1 || prop.types.indexOf('number') != -1) {
+      //         column.type = 'numeric';
+      //       } else if (prop.types.indexOf('boolean') != -1 || prop.types.indexOf('bool') != -1) {
+      //         column.type = 'checkbox';
+      //       } else if (prop.schema.format == 'date' || prop.schema.format == 'date-time') {
+      //         column.type = 'date';
+      //         column.dateFormat = 'YYYY-MM-DD';
+      //         column.correctFormat = true;
+      //       } else if (prop.schema.format == 'country-code') {
+      //         var countries = [];
+      //         for (var cc in metadata.countries) {
+      //           var c = metadata.countries[cc];
+      //           countries.push(c);
+      //         }
+      //         column.type = 'handsontable',
+      //         column.handsontable = {
+      //           data: countries,
+      //           strict: true
+      //         }
+      //       }
+      //       // TODO countries
+      //     }
+      //     // TODO inline objects
+      //     // TODO named objects
+      //     columns.push(column);
+      //   }
+      //   return columns;
+      // };
+
       var getModelColumns = function(model, metadata) {
-        var columns = [],
-            properties = model.getPropertyModels().sort(spindleModelSort);
-        for (var i in properties) {
-          var prop = properties[i];
-          if (prop.schema.hidden || prop.isArray) {
-            continue;
-          }
-          var column = {
-            title: prop.getTitle(),
-            data: prop.name,
-            readOnly: !$scope.editable
-          };
-          if (prop.isValue) {
-            if (prop.types.indexOf('integer') != -1 || prop.types.indexOf('number') != -1) {
-              column.type = 'numeric';
-            } else if (prop.types.indexOf('boolean') != -1 || prop.types.indexOf('bool') != -1) {
-              column.type = 'checkbox';
-            } else if (prop.schema.format == 'date' || prop.schema.format == 'date-time') {
-              column.type = 'date';
-              column.dateFormat = 'YYYY-MM-DD';
-              column.correctFormat = true;
-            } else if (prop.schema.format == 'country-code') {
-              var countries = [];
-              for (var cc in metadata.countries) {
-                var c = metadata.countries[cc];
-                countries.push(c);
-              }
-              column.type = 'handsontable',
-              column.handsontable = {
-                data: countries,
-                strict: true
-              }
-            }
-            // TODO countries
-          }
-          // TODO inline objects
-          // TODO named objects
-          columns.push(column);
-        }
+        var columns = [], models = model.getPropertyModels().sort(spindleModelSort);
+        for (var i in models) {
+          var model = models[i];
+          columns.push({
+            title: model.getTitle(),
+            model: model
+          });
+        } 
         return columns;
       };
 
       var init = function() {
         loadData();
         metadataService.get().then(function(metadata) {
-          $scope.model.columns = getModelColumns($scope.model, metadata);
+          $scope.columns = getModelColumns($scope.model, metadata);
         });
       }
 
