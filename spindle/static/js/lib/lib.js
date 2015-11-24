aab1f118c2676b41305ab3e42a367f4dc0e2ac3d
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-24 11:09:09 +0100

    include raw config.

--- spindle/static/js/lib/lib.js
+++ spindle/static/js/lib/lib.js
@@ -2,14 +2,14 @@ var libSpindleConfig = angular.module('libSpindleConfig', []);
 var SpindleConfig = SpindleConfig || {};
 
 libSpindleConfig.factory('spindleConfig', function() {
-  console.log('Init config:', SpindleConfig);
+  console.log('SpindleConfig:', SpindleConfig);
   return {
     apiUrl: SpindleConfig.apiUrl || '/api',
     metadata: SpindleConfig.metadata,
     entities: SpindleConfig.entities || [],
     schemas: SpindleConfig.schemas || {},
+    raw: SpindleConfig
   };
 });
 
 var libSpindle = angular.module('libSpindle', ['libSpindleConfig']);
-
